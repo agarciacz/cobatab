d235c9504199b3360efb87e1ebefcf82aa2844e6
Alvaro Gilberto Garcia Cruz <alvarogarciia163@gmail.com>
2018-10-16 00:50:05 -0500

    Se creo la funcion para guardar la noticia e imagenes

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Notice;
-use App\User;
+use App\ImageNotice;
 
 
 class NoticeController extends Controller
@@ -19,12 +19,56 @@ class NoticeController extends Controller
         return view('notice.create');
     }
 
-    public function savevideo(Request $request)
+    public function save_notice(Request $request)
     {
-        $validateData = $this->validate($request, [
+        $validatedData = $this->validate($request, [
             'title' => 'required|min:5',
+            'subtitle' => 'required',
             'description' => 'required',
-            'image' => 'required|mimes:jpeg,jpg,png'
+            'cover_image' => 'required|mimes:jpeg,jpg,png',
+            'start_date_publication' => 'required',
+            'end_date_publication' => 'required',
+        ]);
+
+        $notice = new Notice();
+        $user = \Auth::user();
+        $notice->title = $request->input('title');
+        $notice->subtitle = $request->input('subtitle');
+        $notice->description = $request->input('description');
+        //Subir cover_image
+        $cover_image = $request->file('cover_image');
+        if ($cover_image) {
+            $cover_image_path = time() . $cover_image->getClientOriginalName();
+            \Storage::disk('images_notices')->put($cover_image_path, \File::get($cover_image));
+            $notice->cover_image = $cover_image_path;
+        }
+
+        $notice->user = $user->user;
+        $notice->start_date_publication = $request->input('start_date_publication');
+        $notice->end_date_publication = $request->input('end_date_publication');
+        $notice->save();
+
+        $images = $request->file('image');
+        if ($images) {
+            foreach ($images as $image){
+                $images_path = time() . $image->getClientOriginalName();
+                \Storage::disk('images_notices')->put($images_path, \File::get($image));
+                //$notice->cover_image = $images_path;
+                $this->save_images_notices($images_path, $notice);
+            }
+        }
+
+        return redirect()->route('view_list_notice')->with(array(
+            'message' => 'Noticia creada correctamente!!'
+        ));
+
+    }
+
+    public function save_images_notices($image, $id_notice)
+    {
+        ImageNotice::create([
+            'notice' => $id_notice->id,
+            'image' => $image
         ]);
     }
 }

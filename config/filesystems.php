189467d886544698bc1611cac6ceafb30ad93a39
Alvaro Gilberto Garcia Cruz <alvarogarciia163@gmail.com>
2018-10-15 22:56:09 -0500

    Se crea disco para guardar images_notices

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,14 @@ return [
             'visibility' => 'public',
         ],
 
+        'images_notices' => [
+            'driver' => 'local',
+            'root' => storage_path('app/images_notices'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),

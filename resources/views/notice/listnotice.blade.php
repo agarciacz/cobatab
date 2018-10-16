7658ee6777b2482ff141df0106eaf6e55d825068
Alvaro Gilberto Gracia Cruz <alvarogarciia163@gmail.com>
2018-10-15 21:23:56 -0500

    Se creo view para listar las noticias

---
+++ resources/views/notice/listnotice.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.plantilla-admin')
+
+@section('contenido')
+    <div class="row">
+        <section class="col-md-12">
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Noticias</h3>
+                </div>
+                <div class="box-body">
+                    <div class="container">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Titulo</th>
+                                    <th>Elaboro</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection

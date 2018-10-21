0f34169e91be1dbc539396cd57d326f00c247ff5
Alvaro Gilberto Garcia Cruz <alvarogarciia163@gmail.com>
2018-10-21 13:33:19 -0500

    Se actualizo seeds de usuario

---
+++ database/seeds/UsuarioPrincipal.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsuarioPrincipal extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Alvaro Gilberto',
+            'paterno' => 'Garcia',
+            'materno' => 'Cruz',
+            'user' => 'agarcia',
+            'email' => 'alvaro.gcruz@gmail.com',
+            'password' => Hash::make('administrador'),
+        ]);
+
+        $this->command->info('Usuario principal creado');
+    }
+}
